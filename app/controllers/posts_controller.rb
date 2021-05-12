4fbcd9c9766d0b66ed405e45b4a3cacc630c5ca6
ymasaki-dot <y.masaki0526@gmail.com>
2021-05-12 03:23:37 +0000

    ランキング機能終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ApplicationRecord
   attachment :post_image
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
