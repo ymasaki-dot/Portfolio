e798a911a49007a85cf6e8d2b1860cb5cd472ac4
ymasaki-dot <y.masaki0526@gmail.com>
2021-05-21 01:46:13 +0000

    お問合せ機能完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'posts#index'
     resources :posts do
@@ -8,4 +7,7 @@ Rails.application.routes.draw do
     get :search, on: :collection
   end
   resources :users, only: [:show, :edit, :update]
+  get   'inquiry'         => 'inquiry#index'     # 入力画面
+  post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
 end
